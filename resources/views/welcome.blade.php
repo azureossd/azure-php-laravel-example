34cdda33490e1cf580a20f370a0a2255f5a6e03f
Christopher Maldonado <chmald@microsoft.com>
2022-07-05 12:12:53 -0500

    updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome Laravel !
+                    Welcome Laravel ! 07-05-2022
                 </div>
 
                 <div class="links">
